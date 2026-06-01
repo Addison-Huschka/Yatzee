722e485e218ad3f0d796099f50d9bb3179874434
Addison Huschka <yg0531qj@go.minnstate.edu>
2026-05-31 22:01:05 -0500

    setup for diceset

---
+++ DiceSet.java
@@ -0,0 +1,15 @@
+public class DiceSet {
+    private int[] dice = new int[5];
+    public void rollAll(){
+
+    }
+    public void rollSelected(int[] selectedDice){
+
+    }
+    public int[] getDice(){
+        return dice;
+    }
+    public void resetHolds(){
+
+    }
+}
--- Game.java
+++ Game.java
@@ -1,5 +1,14 @@
 public class Game {
     private String Player;
     private int roundnumber;
-    
+    private DiceSet diceset;
+    public void startGame(){
+
+    }
+    public void playRound(){
+
+    }
+    public boolean isGameOver(){
+        return false;
+    }
 }
