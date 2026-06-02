ff9fb05f6f16b88103e16c61d264059a9af34265
Addison Huschka <yg0531qj@go.minnstate.edu>
2026-06-02 12:51:24 -0500

    changed where main is

--- Game.java
+++ Game.java
@@ -2,8 +2,7 @@ public class Game {
     private String Player;
     private int roundnumber;
     private DiceSet diceset;
-    public static void main(String[] args){
-        System.out.println("Welcome to Yatzee!");
+    public void startTheGame(){
 
     }
     public void playRound(){
